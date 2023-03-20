c440f8f50abd23a6f124459d563f801e16d3b78c
megajc <46426490+megajc@users.noreply.github.com>
2023-03-19 22:46:20 -0300

    the digimon baby 1 items description says which digimon they descend from

--- src/main/java/net/modderg/thedigimod/events/BothEventBusSubscriber.java
+++ src/main/java/net/modderg/thedigimod/events/BothEventBusSubscriber.java
@@ -2,21 +2,19 @@ package net.modderg.thedigimod.events;
 
 import jdk.jfr.Registered;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.entity.vehicle.Minecart;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.modderg.thedigimod.TheDigiMod;
 import net.modderg.thedigimod.particles.ModParticle;
 import net.modderg.thedigimod.particles.custom.DigitroneParticles;
 
-import java.rmi.registry.Registry;
-
-@Mod.EventBusSubscriber(modid = TheDigiMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.Mod)
+@Mod.EventBusSubscriber(modid = TheDigiMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BothEventBusSubscriber {
 
     @SubscribeEvent
-    public static void registerParticleFactories(final ParticleFactoryRegisterEvent event){
-        Minecraft.getInstance().particleEngine.register(ModParticles.DIGITRONE_PARTICLES.get(),
+    public static void registerParticleFactories(final RegisterParticleProvidersEvent event){
+        Minecraft.getInstance().particleEngine.register(ModParticle.DIGITRON_PARTICLES.get(),
                 DigitroneParticles.Provider::new);
     }
 
--- src/main/java/net/modderg/thedigimod/particles/ModParticle.java
+++ src/main/java/net/modderg/thedigimod/particles/ModParticle.java
@@ -6,15 +6,16 @@ import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.modderg.thedigimod.TheDigiMod;
 import org.apache.http.config.Registry;
 
 public class ModParticle {
     public static final DeferredRegister<ParticleType<?>> PARTICLE_TYPES =
-            DEFERRED_REGISTER.create(ForgeRegistries.PARTICLE_TYPES,TheDigiMod.MOD_ID);
+            DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES,TheDigiMod.MOD_ID);
 
-    public static final RegisterObject<SimpleParticleType> DIGITRON_PARTICLES =
-            PARTICLE_TYPES.register(bus "digitron_particles", () -> new SimpleParticleType(true));
+    public static final RegistryObject<SimpleParticleType> DIGITRON_PARTICLES =
+            PARTICLE_TYPES.register( "digitron_particles", () -> new SimpleParticleType(true));
     public static void register(IEventBus eventBus){
         PARTICLE_TYPES.register(eventBus);
     }
